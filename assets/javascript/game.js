90b7bf7c3a5a74933fdc3ff5e61ade437441b2eb
sarahg813 <goon.sarah@gmail.com>
2018-06-09 22:45:00 -0700

    added comments to .js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
+// words list
 var words = ["RUGRATS", "SPONGEBOB", "POKEMON", "ANIMANIACS", "RECESS", "DOUG", "CATDOG", "SIMPSONS"];
 
-var maxNumGuesses = 8; 
-var guessedLetters = [];
-var ansWordArr = [];
-var usedWord = [];
-var numGuessesRemaining = 0;
-var numWins = 0;
-var numLosses = 0;
-var isFinished = false;
-var ansWord;
+var maxNumGuesses = 8; // max number of guesses 
+var guessedLetters = []; // store the guessed letters
+var ansWordArr = []; // store the "_" and to be used to replace the word answer
+var numGuessesRemaining = 0; // number of guesses remaining
+var numWins = 0; // number of wins
+var numLosses = 0; // number of losses
+var isFinished = false; // when true, game can start again
+var ansWord; // the word that is being played
 
-
-function restart() {
+// function runs at the start of page and used to restart after game isFinished
+function setup() {
     //picks random word from words list
     ansWord = words[Math.floor(Math.random() * words.length)];
 
@@ -26,13 +26,16 @@ function restart() {
     numGuessesRemaining = maxNumGuesses;
     guessedLetters = [];
 
+    //clears giphy-embed to now show any gifs
     document.getElementById("giphy-embed").src = "";
+    //removes color from numGuesses
     document.getElementById("numGuesses").style.color = "";
 
     //show the selected elements on the screen 
     updateScreen();
 };
 
+//updates the HTML from the functions
 function updateScreen() {
     document.getElementById("numWins").innerText = numWins;
     document.getElementById("numLosses").innerText = numLosses;
@@ -42,16 +45,20 @@ function updateScreen() {
 
 };
 
+//function to check the key that's pressed
 function checkGuess(letter) {
-
+    //if letter is not in guessedLetters array then push the letter to the array
     if (guessedLetters.indexOf(letter) === -1) {
         guessedLetters.push(letter);
+        //if the letter isn't in the answer word then -1 the numGuessesRemaining
         if (ansWord.indexOf(letter) === -1) {
             numGuessesRemaining--;
+            //if numGuessesRemaining is 3 or less then change the color
             if (numGuessesRemaining <=3) {
                 document.getElementById("numGuesses").style.color = "#e12d2e";
             }
-        } else {
+            //if letter is in answer then replace the positioned "_" with the letter
+        } else { 
             for (var i = 0; i < ansWord.length; i++) {
                 if (letter === ansWord[i]) {
                     ansWordArr[i] = letter;
@@ -62,12 +69,13 @@ function checkGuess(letter) {
 
 }; 
 
-
-
+//function to check if the player is a winner
 function isWinner() {
+    //if there are no more "_" in the ansWordArr then +1 to Wins and switch isFinished to true
     if (ansWordArr.indexOf("_") === -1) {
         numWins++;
         isFinished = true;
+        //if the answer is guessed then play assigned gif
         if(ansWord === "DOUG") {
             document.getElementById("giphy-embed").src = "https://giphy.com/embed/w7iOaLoi84N6E";
         } else if (ansWord === "RUGRATS") {
@@ -88,11 +96,13 @@ function isWinner() {
             
     }
 };
-
+//function to check if player is a loser
 function isLoser() {
+    // if the numGuessesRemaining is 0 then -1 numLosses and switch isFinished to true
     if(numGuessesRemaining <= 0) {
         numLosses++;
         isFinished = true;
+        //play the loser gif
         document.getElementById("giphy-embed").src = "https://giphy.com/embed/3oFzmko6SiknmpR2NO";
         document.getElementById("numLosses").style.color = "#e12d2e";
     }
@@ -100,14 +110,18 @@ function isLoser() {
 };
 
 
-
+//event listener for key pressed
 document.onkeyup = function(event) {
+    //if isFinished is true then restart the game to the initial setup 
+    //and switch isFinished back to false
     if (isFinished) {
-        restart();
+        setup();
         isFinished = false;
     } else {
+        //check to see if only letters A-Z are pressed
+        //functions are executed when user presses A-Z key
         if(event.keyCode >= 65 && event.keyCode <= 90) {
-            checkGuess(event.key.toUpperCase());
+            checkGuess(event.key.toUpperCase()); 
             updateScreen();
             isWinner();
             isLoser();
@@ -116,7 +130,7 @@ document.onkeyup = function(event) {
 };
 
 
-restart();
+setup();
 updateScreen();
 
 console.log(ansWord);
